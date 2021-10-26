e98d50ae649ebfdd5809ce4c152b3c239a644b6f
TB831 <druu831@gmail.com>
2021-10-26 15:12:02 -0700

    Revert "first commit. dependency install. hello w0rld"
    This reverts commit 1f54762eecebe71e54da482f541c3d93e03885d0.

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use("/", (req, res) => {
-  res.send("<h1>Hello W0rld</h1>");
-  console.log("Hello W0rld");
-});
-
-app.listen(1234);
